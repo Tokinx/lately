d578cea7bb3637b32d59cfeaa6cf2e4d837f2d00
Cheng <idevs@foxmail.com>
2020-06-23 10:20:54 +0800

    Version 2.0.0

--- lately.js
+++ lately.js
@@ -1,9 +1,8 @@
 /**
  * Lately.js is a jQuery plugin that makes it easy to support automatically
  *
- * @name ViewImage.js
- * @version 1.0.2
- * @requires jQuery v2.0+
+ * @name Lately.js
+ * @version 2.0.0
  * @author Tokin (Tokinx)
  * @license MIT License - http://www.opensource.org/licenses/mit-license.php
  *
@@ -12,56 +11,56 @@
  *
  * Copyright (c) 2017, Biji.IO
  */
-(function ($) {
-    $.extend({
-        lately: function (options) {
-            var setting = $.extend({
-                    'target': '.time',
-                    'lang': {
-                        'second': '秒',
-                        'minute': '分钟',
-                        'hour': '小时',
-                        'day': '天',
-                        'month': '个月',
-                        'year': '年',
-                        'ago': '前',
-                        'error': 'NaN',
-                    }
-                }, options),
-                contains = $(setting.target);
-            for (var i = 0; i < contains.length; i++) {
-                var contain = $(contains[i]),
-                    date = '';
-                if ($(contain).is(":visible")) {
-                    var datetime = $(contain).attr("datetime"),
-                        title = $(contain).attr("title"),
-                        htmls = $(contain).html();
-                    if (datetime ? !isNaN(new Date(datetime = (datetime.replace(/(.*)[a-z](.*)\+(.*)/gi, "$1 $2")).replace(/-/g, "/"))) : false) date = datetime;
-                    else if (title ? !isNaN(new Date(title = title.replace(/-/g, "/"))) : false) date = title;
-                    else if (htmls ? !isNaN(new Date(htmls = htmls.replace(/-/g, "/"))) : false) date = htmls;
-                    else return;
-                    $(contain).html(lately_count(date));
-                }
-            }
-            function lately_count(date) {
-                var date = new Date(date),
-                    second = (new Date().getTime() - date.getTime()) / 1000,
-                    minute = second / 60,
-                    hour = minute / 60,
-                    day = hour / 24,
-                    month = day / 30,
-                    year = month / 12,
-                    floor = Math.floor,
-                    result = '';
-                if (year >= 1) result = floor(year) + setting.lang.year;
-                else if (month >= 1) result = floor(month) + setting.lang.month;
-                else if (day >= 1) result = floor(day) + setting.lang.day;
-                else if (hour >= 1) result = floor(hour) + setting.lang.hour;
-                else if (minute >= 1) result = floor(minute) + setting.lang.minute;
-                else if (second >= 1) result = floor(second) + setting.lang.second;
-                else result = setting.lang.error;
-                return result + setting.lang.ago;
-            }
+; (function (global, undefined) {
+    "use strict"
+    let _global;
+    let Lately = (option) => {
+        let target = option.target || ".time";
+        let lang = option.lang || {
+            'second': '秒',
+            'minute': '分钟',
+            'hour': '小时',
+            'day': '天',
+            'month': '个月',
+            'year': '年',
+            'ago': '前',
+            'error': 'NaN',
+        };
+        let _count = (date) => {
+            date = new Date(date);
+            let second = (new Date().getTime() - date.getTime()) / 1000,
+                minute = second / 60,
+                hour = minute / 60,
+                day = hour / 24,
+                month = day / 30,
+                year = month / 12,
+                floor = (num, _lang) => Math.floor(num) + _lang,
+                result = '';
+            if (year >= 1) result = floor(year, lang.year);
+            else if (month >= 1) result = floor(month, lang.month);
+            else if (day >= 1) result = floor(day, lang.day);
+            else if (hour >= 1) result = floor(hour, lang.hour);
+            else if (minute >= 1) result = floor(minute, lang.minute);
+            else if (second >= 1) result = floor(second, lang.second);
+            else result = lang.error;
+            return result + lang.ago;
         }
-    });
-})(jQuery);
+        addEventListener("DOMContentLoaded", () => {
+            for (let contain of document.querySelectorAll(target)) {
+                let date = '',
+                    date_time = contain.dateTime,
+                    title = contain.title,
+                    html = contain.innerHTML;
+                if (date_time ? !isNaN(new Date(date_time = (date_time.replace(/(.*)[a-z](.*)\+(.*)/gi, "$1 $2")).replace(/-/g, "/"))) : false) date = date_time;
+                else if (title ? !isNaN(new Date(title = title.replace(/-/g, "/"))) : false) date = title;
+                else if (html ? !isNaN(new Date(html = html.replace(/-/g, "/"))) : false) date = html;
+                else return;
+                contain.title = date;
+                contain.innerHTML = _count(date);
+            }
+        })
+    }
+
+    _global = (function () { return this || (0, eval)('this'); }());
+    !('Lately' in _global) && (_global.Lately = Lately);
+}());
